b0fb54e9f682b0b86f8f62aa3cb6ee9314fdfc7c
Karim Osman <kareemosman199@gmail.com>
2023-07-24 21:48:30 +0300

    outline

--- main.go
+++ main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Address struct {
+	City    string      `json:"city"`
+	State   string      `json:"state"`
+	Country string      `json:"country"`
+	Pincode json.Number `json:"pincode"`
+}
+
+type User struct {
+	Name    string      `json:"name"`
+	Age     json.Number `json:"age"`
+	Contact string      `json:"contact"`
+	Company string      `json:"company"`
+	Address Address     `json:"address"`
+}
+
+func main() {
+	dir := "./"
+
+	db, err := New(dir, nil)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+}
