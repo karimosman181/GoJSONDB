5fa3fb01e223459e3b310d392e8adbe25941aefe
Karim Osman <kareemosman199@gmail.com>
2023-07-26 22:11:09 +0300

    add delete func

--- main.go
+++ main.go
@@ -182,7 +182,28 @@ func (d *Driver) ReadAll(collection string) ([]string, error) {
 }
 
 // delete record from database
-func (d *Driver) Delete() error {}
+func (d *Driver) Delete(collection, resource string) error {
+
+	path := filepath.Join(collection, resource)
+
+	mutex := d.getorCreateMutex(collection)
+
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, path)
+
+	switch fi, err := stat(dir); {
+	case fi == nil, err != nil:
+		return fmt.Errorf("Unable to find file or directory named %v\n", path)
+	case fi.Mode().IsDir():
+		return os.RemoveAll(dir)
+	case fi.Mode().IsRegular():
+		return os.RemoveAll(dir + ".json")
+	}
+
+	return nil
+}
 
 // get or create mutex if not exists
 func (d *Driver) getorCreateMutex(collection string) *sync.Mutex {
